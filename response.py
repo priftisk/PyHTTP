5bb2c0e087d1222ad675cdd46a202f1cedbe686d
Konstantinos Priftis <priftis.kostas1997@gmail.com>
2025-11-13 21:43:09 +0200

    Basic response

--- response.py
+++ response.py
@@ -1,12 +1,25 @@
 class Response:
     def __init__(self, request):
-        self.__request= request
-    
+        self.__request = request
+        self.__http_version = "HTTP/1.1"
+        self.__status_code = 200
+        self.__status_message = "OK"
+        self.__headers = {
+            "Content-Type": "text/html; charset=utf-8"
+        }
+        self.__body = "<html><body><h1>Hello World</h1></body></html>"
+
     def encode(self):
-        return """
-HTTP/1.1 201 Created
-Content-Type: application/html
+        # Compute content length
+        body_bytes = self.__body.encode("utf-8")
+        self.__headers["Content-Length"] = str(len(body_bytes))
+        # Build the header section
+        header_str = "\r\n".join(f"{k}: {v}" for k, v in self.__headers.items())
 
-<html><body>Hello World</body></html>
+        # Combine everything properly
+        response = (
+            f"{self.__http_version} {self.__status_code} {self.__status_message}\r\n"
+            f"{header_str}\r\n\r\n"
+        ).encode("utf-8") + body_bytes
 
-""".encode()
+        return response
